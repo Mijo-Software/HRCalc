2b4682f8ae654325646661f0cce26a81010d0c7a
Michael Johne <mjohne@users.noreply.github.com>
2019-12-19 11:13:54 +0100

    Add files via upload

---
+++ HRCalc/MainForm.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace HRCalc
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ HRCalc/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace HRCalc
+{
+	internal static class Program
+	{
+		/// <summary>
+		/// main entrance point of the application
+		/// </summary>
+		[STAThread]
+		private static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(defaultValue: false);
+			using (MainForm mainForm = new MainForm())
+			{
+				Application.Run(mainForm: mainForm);
+			}
+		}
+	}
+}
